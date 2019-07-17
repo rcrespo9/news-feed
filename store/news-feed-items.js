84aba96b68b5d34966d73d74fbdec948c38b8448
Rudy Crespo <crespor@montclair.edu>
2019-07-17 16:28:08 -0400

    load more headlines

--- store/news-feed-items.js
+++ store/news-feed-items.js
@@ -6,13 +6,21 @@ export const state = () => ({
   query: '',
   category: '',
   country: 'us',
-  language: 'en',
-  isMoreResults: false
+  language: 'en'
 })
+
+export const getters = {
+  isMoreResults: (state) => {
+    return state.totalResults !== state.newsFeedItems.length
+  }
+}
 
 export const mutations = {
   SET_NEWS_ITEMS(state, items) {
     state.newsFeedItems = items
+  },
+  ADD_MORE_NEWS_ITEMS(state, items) {
+    state.newsFeedItems.concat(items)
   },
   SET_TOTAL_RESULTS(state, totalResults) {
     state.totalResults = totalResults
@@ -35,10 +43,21 @@ export const actions = {
         }
       )
 
-      commit('SET_NEWS_ITEMS', articles)
-      commit('SET_TOTAL_RESULTS', totalResults)
+      if (page > 1) {
+        commit('ADD_MORE_NEWS_ITEMS', articles)
+      } else {
+        commit('SET_NEWS_ITEMS', articles)
+        commit('SET_TOTAL_RESULTS', totalResults)
+      }
     } catch (error) {
       throw new Error(error)
+    }
+  },
+
+  loadMoreHeadlines({ commit, dispatch, getters }) {
+    if (getters.isMoreResults) {
+      dispatch('fetchHeadlines')
+      commit('INCREMENT_PAGE_NUM')
     }
   }
 }
